26d007692e049df79dd8797bf1e37919b932d1c1
Emmanuel Pereira <emmanuel@timais.com>
2023-08-20 17:35:25 -0300

    ajuste de xp de audio para ser mais justo

--- Services/GuildPolicyService.cs
+++ Services/GuildPolicyService.cs
@@ -378,10 +378,12 @@ namespace DiscordBot.Services
 
                         var qtdPessoasEntraramNaMesmaEpoca = voice.ConnectedUsers.Where(a => ((a.JoinedAt - user.JoinedAt) ?? TimeSpan.Zero).Duration() <= TimeSpan.FromDays(21)).Count();
                         var outrasPessoas = voice.Users.Count - qtdPessoasEntraramNaMesmaEpoca;
-                        if (qtdPessoasEntraramNaMesmaEpoca > 2)
+                        if (qtdPessoasEntraramNaMesmaEpoca > outrasPessoas)
                         {
                             earnedXp /= qtdPessoasEntraramNaMesmaEpoca;
 
+                            if (earnedXp == 0)
+                                earnedXp = 1;
                             Console.WriteLine($"dividido por {qtdPessoasEntraramNaMesmaEpoca}: {member.MemberUserName}");
                         }
                         member.XP += earnedXp;
