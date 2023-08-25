e43668bd4cc9de9200f9802316dab81bef1b886a
Emmanuel Pereira <emmanuel@timais.com>
2023-08-25 19:12:12 -0300

    aaaa

--- Services/MinigameService.cs
+++ Services/MinigameService.cs
@@ -262,7 +262,7 @@ namespace DiscordBot.Services
                     Messages = new[]
                     {
                     new ChatMessage("system", $@"
-Crie um jogo de adivinhação de uma única palavra não-composta e aleatória, que seja necessário ter um bom nível de conhecimento em {ciencias[r]} e que seja indiscutivelmente verdade. Sempre dê três dicas e dê a resposta em seguida no formato:
+Crie um jogo de adivinhação de uma única palavra não-composta e aleatória, que seja necessário ter um bom nível de conhecimento em {ciencias[r]} e que seja indiscutivelmente verdade. Sempre dê três dicas que sejam absolutamente verdade em relação à resposta e dê a resposta em seguida no formato:
 Dicas: 
 - {{dica 1}}
 - {{dica 2}}
